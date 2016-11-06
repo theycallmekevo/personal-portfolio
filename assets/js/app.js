3badd0f2e694df025162f6bb1dfd1aede290c003
Kevin Tran <kevin2005tran@gmail.com>
2016-11-06 10:07:11 -0800

    jquery practice on portfolio page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,72 @@
-// sanity check
-console.log("app.js linked.");
+$( document ).ready(function(){
+
+// $('.hamburger').mouseover(function( event ){
+//   $('.hamburger').hide( 'slow');
+// });
+
+
+// $('.hamburger').mouseover(function(){
+//   $('.hamburger').addClass('test');
+// });
+//
+// $('.hamburger').mouseleave(function(){
+//   $('.hamburger').removeClass('test');
+// });
+
+
+// $('.hamburger').on('mouseover', function(){
+//   $('.hamburger').css('background-color', 'orange');
+// })
+//
+// $('.hamburger').on('mouseleave', function(){
+//   $('.hamburger').css('background-color', '');
+// })
+
+
+// Using multiple unit types within one animation.
+// $( "#go" ).click(function() {
+//   $( "#block" ).animate({
+//     width: "70%",
+//     opacity: 0.4,
+//     marginLeft: "0.6in",
+//     fontSize: "3em",
+//     borderWidth: "10px"
+//   }, 1500 );
+// });
+
+//
+// $( "#go1" ).click(function() {
+//   $( "#block1" )
+//     .animate({
+//       width: "90%"
+//     }, {
+//       queue: false,
+//       duration: 3000
+//     })
+//     .animate({ fontSize: "24px" }, 1500 )
+//     .animate({ borderRightWidth: "15px" }, 1500 );
+// });
+//
+// $( "#go2" ).click(function() {
+//   $( "#block2" )
+//     .animate({ width: "90%" }, 1000 )
+//     .animate({ fontSize: "24px" }, 1000 )
+//     .animate({ borderLeftWidth: "15px" }, 1000 );
+// });
+//
+// $( "#go3" ).click(function() {
+//   $( "#go1" ).add( "#go2" ).click();
+// });
+//
+// $( "#go4" ).click(function() {
+//   $( "div" ).css({
+//     width: "",
+//     fontSize: "",
+//     borderWidth: ""
+//   });
+// });
+
+
+
+
+});
